7264148e1f31c55d2d3aa6055285cf5b7866b418
Juuso Silvennoinen <32332926+Jsos17@users.noreply.github.com>
2018-11-15 23:49:47 +0200

    Added a test for getting entries from database

--- src/test/java/vinkkeri/database/TipDaoTest.java
+++ src/test/java/vinkkeri/database/TipDaoTest.java
@@ -1,40 +1,45 @@
 package vinkkeri.database;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
-import org.junit.AfterClass;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import vinkkeri.objects.Tip;
 
 /**
  *
  * @author jpssilve
  */
 public class TipDaoTest {
-    
-    public TipDaoTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
+
+    TipDao tipDao;
+
     @Before
-    public void setUp() {
+    public void setUp() throws IOException {
+        this.tipDao = new TipDao("jdbc:sqlite:database.db");
+        String date = new java.sql.Date(new java.util.Date().getTime()).toString();
     }
-    
+
+    @Test
+    public void gettingTipsWorks1() throws SQLException, Exception {
+        List<String> tipTitles = new ArrayList<>();
+        for (int i = 0; i < this.tipDao.getTips().size(); i++) {
+            tipTitles.add(this.tipDao.getTips().get(i).getTitle());
+        }
+
+        assertTrue(tipTitles.contains("Jukka Palmun Tuho"));
+        assertTrue(tipTitles.contains("Jukka Palmun Kosto"));
+        assertTrue(tipTitles.contains("Jukka Palmun Seikkailut"));
+    }
+
+    // No Insertion tests yet since no deletion method, and hence database gets cluttered with the same entry everytime the test is run
     @After
     public void tearDown() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
--- src/test/java/vinkkeri/objects/TipTest.java
+++ src/test/java/vinkkeri/objects/TipTest.java
@@ -19,7 +19,7 @@ public class TipTest {
 
     @Before
     public void setUp() {
-        this.tip = new Tip(3, "15/11/2018", "Book", "Introduction to Algorithms", "CLRS", "algos", "9780262033848", "http://mitpress.mit.edu", true);
+        this.tip = new Tip(3, "2018-11-15", "Book", "Introduction to Algorithms", "CLRS", "algos", "9780262033848", "http://mitpress.mit.edu", true);
     }
 
     // Tests below test the get methods of the Tip class, excluding the methods that return lists (no add methods for lists yet)
@@ -28,10 +28,9 @@ public class TipTest {
         assertEquals(3, this.tip.getId());
     }
 
-    // Replace test value with the correct sql formatting
     @Test
     public void getDateTest1() {
-        assertEquals("15/11/2018", this.tip.getDate());
+        assertEquals("2018-11-15", this.tip.getDate());
     }
 
     @Test
