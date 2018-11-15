d382fdc0e049d5642f3c03cf735b680dc616687f
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-11-15 15:18:11 +0200

    Add test test

---
+++ src/test/java/vinkkeri/MainTest.java
@@ -0,0 +1,20 @@
+package vinkkeri;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+	
+	public MainTest() {
+	}
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@Test
+	public void testTest() {
+		assertTrue(true);
+	}
+}
