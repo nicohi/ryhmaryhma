83d0690fd3f4c0351e426de48724f06dbc3c4af8
Nicolas Hiillos <nicolashiillos@gmail.com>
2018-11-26 16:02:15 +0200

    Move Ggui to ListView

--- src/main/java/vinkkeri/main/Main.java
+++ src/main/java/vinkkeri/main/Main.java
@@ -1,24 +1,17 @@
 package vinkkeri.main;
 
-import java.util.Scanner;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import vinkkeri.ui.tui.ConsoleIO;
-import vinkkeri.ui.tui.Controller;
-import vinkkeri.ui.IO;
-import vinkkeri.ui.gui.Details;
-import vinkkeri.ui.gui.Display;
-import vinkkeri.ui.gui.GGui;
-import vinkkeri.ui.tui.Textui;
+import vinkkeri.ui.gui.components.ListView;
 
 public class Main extends Application {
 
     public static void main(String[] args) throws Exception {
- /*---- Tein GGui-luokan, josta löytyy Graafinen käyttöliittymämalli
+ /*---- Tein ListView-luokan, josta löytyy Graafinen käyttöliittymämalli
 Siinä ei ole vielä linkkejä muualle, mutta parissa napissa on hieman toiminnallisuutta
 kommentteja otetaan vastaan :)--- */
-        GGui g = new GGui();
-        Application.launch(GGui.class);
+        ListView g = new ListView();
+        Application.launch(ListView.class);
     }
 
     @Override
